511a46a1dab4069640b1b04f590d6efd7c996df9
Taireru LLC © <tairerullc@gmail.com>
2024-09-16 14:07:01 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+from gitbase.gitbase import GitHubDatabase, PlayerDataSystem
+from cryptography.fernet import Fernet
+
+# Initialize GitHub database and encryption key
+token = "your_github_token"
+repo_owner = "your_repo_owner"
+repo_name = "your_repo_name"
+key = Fernet.generate_key()
+
+db = GitHubDatabase(token, repo_owner, repo_name)
+player_data_system = PlayerDataSystem(db, key)
+
+# Player instance with some attributes
+class Player:
+    def __init__(self, username, score):
+        self.username = username
+        self.score = score
+
+player = Player("john_doe", 100)
+
+# Save specific attributes of the player instance
+player_data_system.save_player_data("john_doe", player, attributes=["username", "score"])
+
+# Load player data
+player_data_system.load_player_data("john_doe", player)
