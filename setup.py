9b9eaecd7816aaabc7ac93a3193794ff4156eed0
Taireru LLC © <tairerullc@gmail.com>
2024-09-25 09:00:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="gitbase",
-    version="0.0.3",
+    version="0.2.2",
     author="Taireru LLC",
     author_email="tairerullc@gmail.com",
-    description="A package to manage player data on GitHub with encryption.",
+    description="A GitHub based database system (Offers offline backups).",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/TaireruLLC/gitbase",
     packages=find_packages(),
     install_requires=[
         "requests",
-        "cryptography"
+        "cryptography",
+        "altcolor"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,4 +21,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    license="MIT",
 )
